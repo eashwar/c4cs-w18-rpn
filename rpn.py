16d9d5594cc0e338d3a56641117a07a8d74a2261
Eashwar Mohan <eashwar@umich.edu>
2018-02-23 11:10:44 -0500

    Add logging

--- rpn.py
+++ rpn.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 import operator
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+sh = logging.StreamHandler(sys.stdout)
+logger.addHandler(sh)
+
 
 operators = {
     '+': operator.add,
@@ -8,6 +16,7 @@ operators = {
     '*': operator.mul,
     '/': operator.truediv,
 }
+
 def calculate(arg):
     stack = list()
 
@@ -21,7 +30,7 @@ def calculate(arg):
             arg1 = stack.pop()
             result = function(arg1, arg2)
             stack.append(result)
-#        print(stack)
+        logger.debug(stack)
   
     if len(stack) != 1:
         raise TypeError
