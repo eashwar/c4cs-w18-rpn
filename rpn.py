c38f6c87a79e3f9b42c5a7eaeb6cd7743a57dea0
Eashwar Mohan <eashwar@umich.edu>
2018-02-23 10:40:11 -0500

    Implement too many args

--- rpn.py
+++ rpn.py
@@ -20,7 +20,10 @@ def calculate(arg):
             result = function(arg1, arg2)
             stack.append(result)
         print(stack)
-   
+  
+    if len(stack) != 1:
+        raise TypeError
+
     return stack.pop()
 
 def main():
