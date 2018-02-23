c70bfa0d2b65e877f5f71baa6f05b129ab95ea3d
Eashwar Mohan <eashwar@umich.edu>
2018-02-23 10:43:42 -0500

    Division and multiplication test. TODO: impl * and /

--- test_rpn.py
+++ test_rpn.py
@@ -9,10 +9,15 @@ class TestBasics(unittest.TestCase):
     def test_adds(self):
         result = rpn.calculate('1 1 + 2 +')
         self.assertEqual(4, result)
-
     def test_subtract(self):
         result = rpn.calculate('5 2 -')
         self.assertEqual(3, result)
     def test_toomany(self):
         with self.assertRaises(TypeError):
             result = rpn.calculate('1 2 3 +')
+    def test_mult(self):
+        result = rpn.calculate('3 2 *')
+        self.assertEqual(6, result)
+    def test_div(self):
+        result = rpn.calculate('6 3 /')
+        self.assertEqual(2, result)
